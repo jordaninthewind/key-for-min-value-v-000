912fff5279d73ec0f605cf61e7faa2250da0acaf
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 05:29:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,7 @@
 def key_for_min_value(name_hash)
   reference = nil
   name_hash.each do | key, val |
-    p name_hash[key]
-    p val
+    p reference
     if reference == nil
       reference = key
     elsif name_hash[reference] < val
